771deb43a188f7fafab5a5e804fb72920e4861aa
HaoAlone <1216787948@qq.com>
2018-06-13 18:27:43 +0800

    bug

--- src/lib/getAVUser.js
+++ src/lib/getAVUser.js
@@ -2,5 +2,8 @@ import AV from './leancloud'
 
 export default function (user) {
   var {id,attributes:{username}} = user || AV.User.current() || {attributes:{}}
-  return {id,name}
+  return {
+    id:id || '',
+    name:name || ''
+  }
 }
